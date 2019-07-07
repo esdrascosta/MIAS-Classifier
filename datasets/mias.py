b2c8207ac35c2d3bec07d1b1f194da1d478d6308
mateusnbm <mnbm@cin.ufpe.br>
2019-07-07 18:48:58 -0300

    Initial commit.

--- datasets/mias.py
+++ datasets/mias.py
@@ -1,142 +1,466 @@
-import torch.utils.data as data
-from six.moves import urllib
-import numpy as np
-import tarfile
-import errno
-import gzip
-import re
+#
+# mias.py
+#
+
 import os
+import re
+import json
+import gzip
+import errno
+import tarfile
+
+import random
+import shutil
+from PIL import Image
+
+from six.moves import urllib
+
+import numpy as np
+import torch.utils.data as data
+
+'''
+
+Class grouping information about an instance of the MIAS dataset.
+
+'''
 
 class MIAS(data.Dataset):
 
-  raw_folder = 'raw'
-  processed_folder = 'processed'
-  dataset_file = 'dataset.pt'
-  urls = {
-    'dataset' : 'http://peipa.essex.ac.uk/pix/mias/all-mias.tar.gz'
-  }
+	root_folder_name = 'data'
+	raw_data_folder_name = 'all-mias'
 
-  def __init__(self, root, transform=None, target_transform=None, download=True):
-    self.root = os.path.expanduser(root)
-    self.transform = transform
-    self.target_transform = target_transform
-    if download:
-      self.download()
+	url_dataset = 'http://peipa.essex.ac.uk/pix/mias/all-mias.tar.gz'
 
-    # TODO proces files to build an unique dataset file
-    self.parse_data()
+	'''
 
-  def __len__(self):
-    return len(self.samples)
+	Class initialization.
 
-  def __getitem__(self, index):
-    """
-    Args:
-      index (int): Index
-    Returns:
-      tuple: (image, target)
-      where target is index of the target class and info contains
-    """
+	'''
 
-    path, target = self.samples[index]
-    sample = self._read_pgm(path)
-    if self.transform is not None:
-        sample = self.transform(sample)
-    if self.target_transform is not None:
-        target = self.target_transform(target)
+	def __init__(self, transform=None, target_transform=None, load=1):
 
-    return sample, target
+		self.transform = transform
+		self.target_transform = target_transform
 
-  def parse_data(self):
-    url = self.urls['dataset']
-    filename = url.rpartition('/')[2]
-    folder_path = os.path.join(self.root, self.raw_folder, filename.replace('.tar.gz', ''))
-    file_names, labels = self._parse_infos(folder_path)
-    self.file_paths = [ os.path.join(folder_path, file_name) for file_name in file_names ]
-    self.labels_info = { v:i for i,v in enumerate( list({ i for i in labels }) ) }
-    self.targets = [ self.labels_info[label] for label in labels ]
-    self.samples = list(zip(self.file_paths, self.targets))
+		# Download the dataset if specified and needed.
 
-  def download(self):
-    if self._check_processed_exists():
-      return  
+		self.__download__(self.url_dataset)
 
-    try:
-      os.makedirs(os.path.join(self.root, self.raw_folder))
-      os.makedirs(os.path.join(self.root, self.processed_folder))
-    except OSError as e:
-      if e.errno == errno.EEXIST:
-        pass
-      else:
-        raise
+		# Parse the dataset information.
 
-    url = self.urls['dataset']
-    filename = url.rpartition('/')[2]
+		p, s, l = self.__parse_data__(self.raw_data_folder_name)
 
-    print('Downloading ' + url)
-    data = urllib.request.urlopen(url)
-    file_path = os.path.join(self.root, self.raw_folder, filename)
-    with open(file_path, 'wb') as f:
-      f.write(data.read())
+		if load == 0:
 
-    tf = tarfile.open(file_path)
-    folder_path = os.path.join(self.root, self.raw_folder, filename.replace('.tar.gz', ''))
-    tf.extractall(folder_path)
-    tf.close()
-    os.unlink(file_path)
+			# Use raw data, full sized images and no data augmentation.
 
-  def get_class(self, index):
-    for name, i in self.labels_info.items():
-      if i == index:
-        return name
+			self.patients = p
+			self.samples = s
+			self.labels_enumeration = l
 
-  def _read_pgm(self, filename, byteorder='>'):                     
-    """Return image data from a raw PGM file as a numpy array.
+		elif load == 1:
 
-    Format specification: http://netpbm.sourceforge.net/doc/pgm.html
+			# Use full sized images and data augmentation.
 
-    """
-    with open("{}.pgm".format(filename), 'rb') as f:
-      buffer = f.read()
-    try:
-      header, width, height, maxval = re.search(  
-      b"(^P5\s(?:\s*#.*[\r\n])*"                             
-      b"(\d+)\s(?:\s*#.*[\r\n])*"                            
-      b"(\d+)\s(?:\s*#.*[\r\n])*"                            
-      b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()       
-                                                            
-    except AttributeError:
-      raise ValueError("Not a raw PGM file: '%s'" % filename)
-      
-    return np.frombuffer(buffer,
-                dtype='u1' if int(maxval) < 256 else byteorder+'u2',
-                count=int(width)*int(height),
-                offset=len(header)
-                ).reshape(( int(height), int(width) ))
+			p, s, l = self.__simple_data_augmentation__(p, l)
 
-  def _parse_infos(self, folder):
-    regex = r"(mdb\d{3})\W([FGD])\W(CALC|CIRC|SPIC|MISC|ARCH|ASYM|NORM)\W?([BM])?\W?((\d{1,3})\W(\d{1,3})\W(\d{1,3})|(\*NOTE\W3\*))?"
-    file_names = []
-    labels = [] 
-    for line in open(os.path.join(folder, 'Info.txt')):
-      matches = re.finditer(regex, line)
-      for matchNum, match in enumerate(matches, start=1):
-        file_names.append(match.group(1))
-        labels.append(match.group(3))
-    
-    return file_names, labels       
+			self.patients = p
+			self.samples = s
+			self.labels_enumeration = l
 
-  def _check_processed_exists(self):
-    return os.path.exists(os.path.join(self.root, self.processed_folder, self.dataset_file))
+		else:
 
-# only for test reasons
+			# Use images containing the ROIs and data augmentation.
+
+			self.patients = p
+			self.samples = s
+			self.labels_enumeration = l
+
+	'''
+
+	Returns the number of samples our dataset has.
+
+	'''
+
+	def __len__(self):
+
+		return len(self.samples)
+
+	'''
+
+	Returns a tuple (image, target) when the getitem accessor is used.
+
+	'''
+
+	def __getitem__(self, index):
+
+		path, target = self.samples[index]
+		sample = self._read_pgm(path)
+
+		if self.transform is not None:
+
+			sample = self.transform(sample)
+
+		if self.target_transform is not None:
+
+			target = self.target_transform(target)
+
+		return sample, target
+
+	'''
+
+	xxx
+
+	'''
+
+	def _read_pgm(self, filename, byteorder='>'):
+
+		"""
+		Return image data from a raw PGM file as a numpy array.
+		Format specification: http://netpbm.sourceforge.net/doc/pgm.html
+		"""
+
+		with open("{}.pgm".format(filename), 'rb') as f:
+
+			buffer = f.read()
+
+		try:
+
+			header, width, height, maxval = re.search(
+				b"(^P5\s(?:\s*#.*[\r\n])*"
+				b"(\d+)\s(?:\s*#.*[\r\n])*"
+				b"(\d+)\s(?:\s*#.*[\r\n])*"
+				b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
+
+		except AttributeError:
+
+			raise ValueError("Not a raw PGM file: '%s'" % filename)
+
+		return np.frombuffer(buffer,
+			dtype='u1' if int(maxval) < 256 else byteorder+'u2',
+			count=int(width)*int(height),
+			offset=len(header)
+			).reshape(( int(height), int(width) ))
+
+	'''
+
+	Downloads the database from a remote location.
+
+	'''
+
+	def __download__(self, url):
+
+		print('Downloading: ' + url)
+
+		download_destination_path = self.root_folder_name
+		download_file_name = url.rpartition('/')[2]
+		download_file_path = os.path.join(download_destination_path, download_file_name)
+
+		download_folder_name = download_file_name.replace('.tar.gz', '')
+		download_folder_path = os.path.join(download_destination_path, download_folder_name)
+
+		try:
+			os.makedirs(download_destination_path)
+		except OSError as e:
+			if e.errno == errno.EEXIST:
+				print('Data folder exists, skipping download.')
+			return
+
+		data = urllib.request.urlopen(url)
+
+		with open(download_file_path, 'wb') as f:
+
+			f.write(data.read())
+
+		tf = tarfile.open(download_file_path)
+		tf.extractall(download_folder_path)
+		tf.close()
+		os.unlink(download_file_path)
+
+	'''
+
+	Extracts information from the dataset 'Infos.txt' file and parses it into
+	an array of dictionaries. Each array item has the 'left' and 'right' keys,
+	referencing the images for the left and right mammograms. Each dictionary
+	contains the image filename,  background tissue, class of abnormality,
+	severity of abnormality, the coordinates and radius of the abnormality.
+
+	'''
+
+	def __parse_mias_infos__(self, folder_path_dataset):
+
+		regex = r"(mdb\d{3})\W([FGD])\W(CALC|CIRC|SPIC|MISC|ARCH|ASYM|NORM)\W?([BM])?\W?((\d{1,3})\W(\d{1,3})\W(\d{1,3})|(\*NOTE\W3\*))?"
+
+		patients = []
+		patient_index = 0
+		patient_rleft = False
+
+		file_names = []
+		file_labels = []
+
+		infos_filename = 'Info.txt'
+		infos_filepath = os.path.join(folder_path_dataset, infos_filename)
+		infos_file = open(infos_filepath)
+
+		for line in infos_file:
+
+			matches = re.finditer(regex, line)
+
+			for matchNum, match in enumerate(matches, start=1):
+
+				img_filename = match.group(1)
+				bck_tissue = match.group(2)
+				abn_class = match.group(3)
+
+				if abn_class != 'NORM':
+
+					abn_severity = match.group(4)
+					line_litems = match.group(5)
+					abn_radius = 0
+					abn_coordinates = (0, 0)
+					abn_has_coordinates = False
+
+					if line_litems != None and line_litems != '*NOTE 3*':
+
+						items = line_litems.split(' ')
+						abn_has_coordinates = True
+						abn_coordinates = (int(items[0]), int(items[1]))
+						abn_radius = int(items[2])
+
+				if patient_rleft == False:
+
+					patients.append({'left':{}, 'right':{}})
+
+					cside = 'left'
+					cindex = patient_index
+					patient_rleft = True
+
+				else:
+
+					cside = 'right'
+					cindex = patient_index
+					patient_rleft = False
+					patient_index = patient_index + 1
+
+				file_names.append(img_filename)
+				file_labels.append(abn_class)
+
+				patients[cindex][cside]['img_filename'] = img_filename
+				patients[cindex][cside]['bck_tissue'] = bck_tissue
+				patients[cindex][cside]['abn_class'] = abn_class
+				patients[cindex][cside]['abn_severity'] = abn_severity
+				patients[cindex][cside]['abn_radius'] = abn_radius
+				patients[cindex][cside]['abn_coordinates'] = abn_coordinates
+				patients[cindex][cside]['abn_has_coordinates'] = abn_has_coordinates
+
+		return file_names, file_labels, patients
+
+	'''
+
+	Parses the information available in the dataset `Infos.txt` file.
+
+	'''
+
+	def __parse_data__(self, folder_name_dataset):
+
+		folder_path_dataset = os.path.join(self.root_folder_name, folder_name_dataset)
+		file_names, file_labels, patients = self.__parse_mias_infos__(folder_path_dataset)
+
+		file_paths = [ os.path.join(folder_path_dataset, fname) for fname in file_names ]
+		labels_enumeration = { v:i for i, v in enumerate( list({ i for i in file_labels }) ) }
+		targets = [ labels_enumeration[label] for label in file_labels ]
+
+		patients = patients
+		samples = list(zip(file_paths, targets))
+		labels_enumeration = labels_enumeration
+
+		return patients, samples, labels_enumeration
+
+	'''
+
+	Performs a simple dataset augmentation. Each image is rotated three times by
+	steps of 90 degrees, flipped horizontally and vertically (five new copies).
+
+	'''
+
+	def __simple_data_augmentation__(self, patients, labels_enumeration):
+
+		p_index = 0
+		n_samples = []
+		n_patients = []
+
+		folder_name_dataset = 'all-augmented'
+		folder_path_dataset = os.path.join(self.root_folder_name, folder_name_dataset)
+
+		infos_filename = 'infos.json'
+		infos_filepath = os.path.join(folder_path_dataset, infos_filename)
+
+		if os.path.isdir(folder_path_dataset) == True:
+
+			infos_file = open(infos_filepath, "r")
+			patients = json.load(infos_file)
+
+			for patient in patients:
+
+				for side in ['left', 'right']:
+
+					abn_class = patient[side]['abn_class']
+					abn_class_enumeration = labels_enumeration[abn_class]
+					img_filename = patient[side]['img_filename']
+					img_path = os.path.join(folder_path_dataset, img_filename)
+
+					n_samples.append((img_path, abn_class_enumeration))
+
+			return patients, n_samples, labels_enumeration
+
+		try:
+			os.makedirs(folder_path_dataset)
+		except OSError as e:
+			if e.errno != errno.EEXIST:
+				raise
+
+		infos_file = open(infos_filepath, "w+")
+
+		for patient in patients:
+
+			n_patients.append({})
+			n_patients.append({})
+			n_patients.append({})
+			n_patients.append({})
+			n_patients.append({})
+
+			for side in ['left', 'right']:
+
+				o_abn_coordinates = patient[side]['abn_coordinates']
+				o_coordinate_x = o_abn_coordinates[0]
+				o_coordinate_y = o_abn_coordinates[1]
+				o_abn_class = patient[side]['abn_class']
+				o_ab_class_enum = labels_enumeration[o_abn_class]
+				o_img_filename = patient[side]['img_filename']
+				o_img_path = os.path.join(self.root_folder_name, self.raw_data_folder_name, o_img_filename)+'.pgm'
+
+				shutil.copy(o_img_path, os.path.join(folder_path_dataset, o_img_filename)+'.pgm')
+
+				n_samples_img_path = os.path.join(folder_path_dataset, o_img_filename)
+				n_samples.append((n_samples_img_path, o_ab_class_enum))
+
+				for i in range(5):
+
+					index = p_index + i
+
+					n_img_filename = o_img_filename + '-' + str(index) + '-' + side
+					n_img_path = os.path.join(folder_path_dataset, n_img_filename)+'.pgm'
+
+					if i == 0:
+						n_coordinate_x = (1024 - o_coordinate_x)
+						n_coordinate_y = o_coordinate_y
+						Image.open(o_img_path).transpose(Image.FLIP_LEFT_RIGHT).save(n_img_path)
+					elif i == 1:
+						n_coordinate_x = o_coordinate_x
+						n_coordinate_y = (1024 - o_coordinate_y)
+						Image.open(o_img_path).transpose(Image.FLIP_TOP_BOTTOM).save(n_img_path)
+					elif i == 2:
+						n_coordinate_x = (1024 - o_coordinate_y)
+						n_coordinate_y = o_coordinate_x
+						Image.open(o_img_path).rotate(90).save(n_img_path)
+					elif i == 3:
+						n_coordinate_x = (1024 - o_coordinate_x)
+						n_coordinate_y = (1024 - o_coordinate_y)
+						Image.open(o_img_path).rotate(180).save(n_img_path)
+					elif i == 4:
+						n_coordinate_x = o_coordinate_y
+						n_coordinate_y = (1024 - o_coordinate_x)
+						Image.open(o_img_path).rotate(270).save(n_img_path)
+
+					n_patients[index][side] = {}
+					n_patients[index][side]['img_filename'] = n_img_filename
+					n_patients[index][side]['abn_coordinates'] = (n_coordinate_x, n_coordinate_y)
+					n_patients[index][side]['bck_tissue'] = patient[side]['bck_tissue']
+					n_patients[index][side]['abn_class'] = patient[side]['abn_class']
+					n_patients[index][side]['abn_severity'] = patient[side]['abn_severity']
+					n_patients[index][side]['abn_radius'] = patient[side]['abn_radius']
+					n_patients[index][side]['abn_has_coordinates'] = patient[side]['abn_has_coordinates']
+
+					n_samples_img_path = os.path.join(folder_path_dataset, n_img_filename)
+					n_samples.append((n_samples_img_path, o_ab_class_enum))
+
+			p_index = p_index + 5
+
+		json.dump((patients + n_patients), infos_file, indent=4)
+
+		return (patients + n_patients), n_samples, labels_enumeration
+
+	'''
+
+	xxx
+
+	'''
+
+	def plot_class_distribution(self):
+
+		print('Plotting class distribution.')
+
+		labels = list(self.labels_enumeration.keys())
+		samples = [sample[1] for sample in self.samples]
+		counts = { labels[i]:samples.count(i) for i in range(0, len(labels))}
+
+		items = [(label, counts[label]) for label in labels]
+		items = sorted(items, key=lambda x: x[1])
+
+		x_labels = [item[0] for item in items]
+		y_pos = np.arange(len(x_labels))
+		y_values = [item[1] for item in items]
+
+		plt.bar(y_pos, y_values, align='center', alpha=0.8)
+		plt.xticks(y_pos, x_labels)
+		plt.ylabel('Ocorrências')
+		plt.title('Distribuição do Banco por Classe')
+		plt.show()
+
+	'''
+
+	Returns the number of classes on the parsed data.
+
+	'''
+
+	def num_classes(self):
+
+		return len(self.labels_enumeration)
+
+	'''
+
+	Translates a given integer to the original class name.
+
+	'''
+
+	def get_class(self, id):
+
+		for name, i in self.labels_enumeration.items():
+
+			if i == id:
+
+				return name
+
+'''
+
+xxx
+
+'''
+
 if __name__ == "__main__":
-  import matplotlib.pyplot as plt
 
-  dataset = MIAS('./data', download=False)
-  img, target = dataset[0]
-  print(target)
-  print(dataset.get_class(target))
-  print(img.shape)
-  plt.imshow(img, cmap='gray')
-  plt.show()
+	# Hack to make matplotlib work on macOS under a virtual enviorment.
+
+	import matplotlib
+	matplotlib.use('TkAgg')
+	import matplotlib.pyplot as plt
+
+	# Initialize the dataset.
+	# If the data doesnt't exist, the class will download it.
+	# If augment is True and data aumentation wasn't performed yet, it will.
+
+	dataset = MIAS()
+
+	# Plot the samples distribution per class.
+
+	dataset.plot_class_distribution()
--- train.py
+++ train.py
@@ -1,4 +1,12 @@
+#
+# train.py
+#
+
 from __future__ import print_function, division
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
@@ -7,7 +15,6 @@ from torch.optim import lr_scheduler
 import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 import time
 import os
 import copy
@@ -16,145 +23,266 @@ from model import ResNet18
 from datasets import MIAS
 from PIL import Image
 
+from sampler import ImbalancedDatasetSampler
+
+'''
+
+Arguments.
+
+'''
+
 parser = argparse.ArgumentParser(description='ResNet-MIAS')
-parser.add_argument('--load-weights', type=str, default=None, metavar='LW',
-                    help='load weights from given file')
-parser.add_argument('--batch-size', type=int, default=10, metavar='N',
-                    help='input batch size for training (default: 20)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
-                    help='learning rate (default: 0.001)')
-parser.add_argument('--cuda', action='store_true', default=True,
-                    help='CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--snapshot-folder', type=str, default='./snapshots', metavar='SF',
-                    help='where to store the snapshots')
-parser.add_argument('--data-folder', type=str, default='./data', metavar='DF',
-                    help='where to store the datasets')
+
+parser.add_argument(
+	'--load-weights',
+	type=str,
+	default=None,
+	metavar='LW',
+	help='load weights from given file'
+	)
+
+parser.add_argument(
+	'--batch-size',
+	type=int,
+	default=20,
+	metavar='N',
+	help='input batch size for training (default: 20)'
+	)
+
+parser.add_argument(
+	'--epochs',
+	type=int,
+	default=10,
+	metavar='N',
+	help='number of epochs to train (default: 10)'
+	)
+
+parser.add_argument(
+	'--lr',
+	type=float,
+	default=0.001,
+	metavar='LR',
+	help='learning rate (default: 0.001)'
+	)
+
+parser.add_argument(
+	'--cuda',
+	action='store_true',
+	default=True,
+	help='CUDA training'
+	)
+
+parser.add_argument(
+	'--seed',
+	type=int,
+	default=1,
+	metavar='S',
+	help='random seed (default: 1)'
+	)
+
+parser.add_argument(
+	'--snapshot-folder',
+	type=str,
+	default='./snapshots',
+	metavar='SF',
+	help='where to store the snapshots'
+	)
+
+parser.add_argument(
+	'--data-folder',
+	type=str,
+	default='',
+	metavar='DF',
+	help='where to store the datasets'
+	)
+
 args = parser.parse_args()
 
-# REPRODUCIBILITY
-torch.manual_seed(args.seed)
+'''
+
+Set a custom seed to ensure reproducibility.
+
+'''
+
 np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+'''
+
+Specify the running device (CPU or GPU with CUDA support).
+
+'''
 
 device = torch.device("cuda:0" if torch.cuda.is_available() and args.cuda else "cpu")
 
-# load dataset
-data_path = os.path.join(args.data_folder)
-full_dataset = MIAS(data_path, download=False, 
-            transform=transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize((128, 128), interpolation=Image.LANCZOS), 
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.ToTensor()
-            ]))    
-num_classes = len(full_dataset.labels_info)
-train_size = int(len(full_dataset) * .7) 
-val_size = len(full_dataset) - train_size
+'''
 
-print(f"Train Size: {str(train_size)}")
-print(f"Val Size: {str(val_size)}")
+Load the dataset.
 
-train_dataset, val_dataset = torch.utils.data.random_split(full_dataset, [train_size, val_size])
+'''
 
-train_loader = torch.utils.data.DataLoader(train_dataset, 
-                batch_size=args.batch_size, shuffle=True)
-val_loader = torch.utils.data.DataLoader(val_dataset, 
-                batch_size=args.batch_size, shuffle=True)
+dataset_folder_path = os.path.join(args.data_folder)
 
-dataloaders = {
-  'train': train_loader,
-  'val': val_loader
-}
+mias_dataset = MIAS(
+	transform=transforms.Compose([
+		transforms.ToPILImage(),
+		transforms.Resize((128, 128), interpolation=Image.LANCZOS),
+		transforms.RandomHorizontalFlip(p=0.5),
+		transforms.ToTensor()
+		])
+	)
 
-dataset_sizes = {
-  'train': train_size,
-  'val': val_size
-}
+'''
+
+Prepare the data.
+
+'''
+
+num_samples = len(mias_dataset)
+num_classes = mias_dataset.num_classes()
+training_batch_size = int(num_samples * 0.7)
+validation_batch_size = (num_samples - training_batch_size)
+
+print(f"Training batch size: {str(training_batch_size)}")
+print(f"Validation batch size: {str(validation_batch_size)}")
+
+training_set, validation_set = torch.utils.data.random_split(
+	mias_dataset,
+	[training_batch_size, validation_batch_size]
+	)
+
+training_loader = torch.utils.data.DataLoader(
+	training_set,
+	batch_size=args.batch_size,
+	shuffle=False,
+	sampler=ImbalancedDatasetSampler(training_set)
+	)
+
+validation_loader = torch.utils.data.DataLoader(
+	validation_set,
+	batch_size=args.batch_size,
+	shuffle=True
+	)
+
+data_loaders = { 'train': training_loader, 'val': validation_loader }
+batch_sizes = { 'train': training_batch_size,'val': validation_batch_size }
+
+'''
+
+Training function.
+
+'''
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs):
-    since = time.time()
 
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-    
-    for epoch in range(num_epochs):
-      # Each epoch has a training and validation phase
-      epoch_since = time.time()
-      for phase in ['train', 'val']:
-           
-        if phase == 'train':
-          scheduler.step()
-          model.train()  # Set model to training mode
-        else:
-          model.eval()   # Set model to evaluate mode
+	since = time.time()
 
-        running_loss = 0.0
-        running_corrects = 0
+	best_acc = 0.0
+	best_model_wts = copy.deepcopy(model.state_dict())
 
-        # Iterate over data.
-        for inputs, labels in dataloaders[phase]:
-          inputs = inputs.to(device)
-          labels = labels.to(device)
+	for epoch in range(num_epochs):
 
-          # zero the parameter gradients
-          optimizer.zero_grad()
+		epoch_since = time.time()
 
-          # forward
-          # track history if only in train
-          with torch.set_grad_enabled(phase == 'train'):
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
+		for phase in ['train', 'val']:
 
-              # backward + optimize only if in training phase
-            if phase == 'train':
-              loss.backward()
-              optimizer.step()
+			running_loss = 0.0
+			running_corrects = 0
 
-          # statistics
-          running_loss += loss.item() * inputs.size(0)
-          running_corrects += torch.sum(preds == labels.data)
+			if phase == 'train':
+				scheduler.step()
+				model.train()
+			else:
+				model.eval()
 
-        epoch_loss = running_loss / dataset_sizes[phase]
-        epoch_acc = running_corrects.double() / dataset_sizes[phase]
+			for inputs, labels in data_loaders[phase]:
 
-        epoch_time_elapsed = time.time() - epoch_since
-        eta = ((num_epochs - epoch) + 1 ) * epoch_time_elapsed
-        print('Epoch: {}/{} Phase: {:<5} Loss: {:.4f} Acc: {:.4f} Epoch Time: {:.0f}s ETA: {:.0f}m {:.0f}s'.format(
-          epoch + 1, num_epochs, phase, epoch_loss, epoch_acc, epoch_time_elapsed, eta // 60, eta % 60))
+				inputs = inputs.to(device)
+				labels = labels.to(device)
 
-        # deep copy the model
-        if phase == 'val' and epoch_acc > best_acc:
-          best_acc = epoch_acc
-          best_model_wts = copy.deepcopy(model.state_dict())
+				# zero the parameter gradients
+				optimizer.zero_grad()
 
-    print()
+				# forward
+				# track history if only in train
+				with torch.set_grad_enabled(phase == 'train'):
 
-    time_elapsed = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+					outputs = model(inputs)
+					_, preds = torch.max(outputs, 1)
+					loss = criterion(outputs, labels)
 
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
+					# backward + optimize only if in training phase
+					if phase == 'train':
+						loss.backward()
+						optimizer.step()
 
-def main(): 
-  model_ft = ResNet18(num_classes=num_classes)
-  print(model_ft)
-  model_ft = model_ft.to(device)
+				# statistics
+				running_loss += loss.item() * inputs.size(0)
+				running_corrects += torch.sum(preds == labels.data)
 
-  criterion = nn.CrossEntropyLoss()
-  optimizer_ft = optim.SGD(model_ft.parameters(), lr=args.lr, momentum=0.9)
+			epoch_loss = running_loss / batch_sizes[phase]
+			epoch_acc = running_corrects.double() / batch_sizes[phase]
+			epoch_time_elapsed = time.time() - epoch_since
 
-  # Decay LR by a factor of 0.1 every 7 epochs
-  exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+			eta = ((num_epochs - epoch) + 1 ) * epoch_time_elapsed
 
-  train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=args.epochs)
+			print('Epoch: {}/{} Phase: {:<5} Loss: {:.4f} Acc: {:.4f} Epoch Time: {:.0f}s ETA: {:.0f}m {:.0f}s'.format(epoch + 1, num_epochs, phase, epoch_loss, epoch_acc, epoch_time_elapsed, eta // 60, eta % 60))
+
+			# deep copy the model
+			if phase == 'val' and epoch_acc > best_acc:
+				best_acc = epoch_acc
+				best_model_wts = copy.deepcopy(model.state_dict())
+
+	time_elapsed = time.time() - since
+
+	print('')
+	print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+	print('Best val Acc: {:4f}'.format(best_acc))
+
+	# load best model weights
+	model.load_state_dict(best_model_wts)
+
+	return model
+
+'''
+
+Main function.
+
+'''
+
+def main():
+
+	model_ft = ResNet18(num_classes=num_classes)
+	model_ft = model_ft.to(device)
+
+	criterion = nn.CrossEntropyLoss()
+
+	optimizer_ft = optim.SGD(
+		model_ft.parameters(),
+		lr=args.lr,
+		momentum=0.9
+		)
+
+	dec_lr_scheduler = lr_scheduler.StepLR(
+		optimizer_ft,
+		step_size=7,
+		gamma=0.1
+		)
+
+	train_model(
+		model_ft,
+		criterion,
+		optimizer_ft,
+		dec_lr_scheduler,
+		num_epochs=args.epochs
+		)
+
+'''
+
+Call main() on the application default entry point.
+
+'''
 
 if __name__ == "__main__":
-    main()
-    
+
+	main()
